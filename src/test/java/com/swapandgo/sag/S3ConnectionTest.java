6bd80af956987b2577382356a88238fe17a29b5c
jangeunho <jangeh3031>
2026-01-06 18:56:29 +0100

    S3연결 테스트

---
+++ src/test/java/com/swapandgo/sag/S3ConnectionTest.java
@@ -0,0 +1,27 @@
+package com.swapandgo.sag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.HeadBucketRequest;
+
+@SpringBootTest
+public class S3ConnectionTest {
+    @Autowired
+    private S3Client s3Client;
+
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
+
+    @Test
+    void S3_연결테스트(){
+        HeadBucketRequest request = HeadBucketRequest.builder()
+                .bucket(bucket)
+                .build();
+
+        s3Client.headBucket(request);
+        System.out.println("연결 성공");
+    }
+}
