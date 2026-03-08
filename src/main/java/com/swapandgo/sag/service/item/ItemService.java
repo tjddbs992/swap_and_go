49b988b9bff66dd00fdd8f12604570812fe8cad8
jangeunho <jangeh3031>
2026-03-08 23:41:33 +0900

    itemService 수정

--- src/main/java/com/swapandgo/sag/service/item/ItemService.java
+++ src/main/java/com/swapandgo/sag/service/item/ItemService.java
@@ -47,28 +47,6 @@ public class ItemService {
         return item.getId();
     }
 
-    public Long saveRentalItem(Long userId, ItemRequest request){
-        User user = userRepository.findById(userId).orElseThrow(
-                () -> new IllegalArgumentException("사용자를 찾을 수 없습니다.")
-        );
-        Item item = Item.create(
-                user,
-                request.getTitle(),
-                request.getContent(),
-                request.getPrice(),
-                request.getDeposit(),
-                ItemType.RENTAL,
-                request.getTradeType(),
-                request.getCategory(),
-                request.getLocation(),
-                request.getImages()
-        );
-
-        itemRepository.save(item);
-
-        return item.getId();
-    }
-
     public Long updateUsedItem(Long userId, Long itemId, ItemRequest request){
         Item findItem = itemRepository.findById(itemId).orElseThrow(
                 () -> new IllegalArgumentException("아이템을 찾을 수 없습니다 : " + itemId));
@@ -88,27 +66,7 @@ public class ItemService {
                 request.getPrice(),
                 null,
                 request.getCategory(),
-                request.getTradeType()
-        );
-
-        return findItem.getId();
-    }
-
-    public Long updateRentalItem(Long userId, Long itemId, ItemRequest request){
-        Item findItem = itemRepository.findById(itemId).orElseThrow(
-                () -> new IllegalArgumentException("아이템을 찾을 수 없습니다 : " + itemId));
-        User user = userRepository.findById(userId).orElseThrow(
-                () -> new IllegalArgumentException("사용자를 찾을 수 없습니다 : " + userId));
-        if (!findItem.isOwner(user))
-            throw new IllegalArgumentException("게시글을 수정할 권한이 없습니다.");
-
-        findItem.update(
-                request.getTitle(),
-                request.getContent(),
-                request.getPrice(),
-                request.getDeposit(),
-                request.getCategory(),
-                request.getTradeType()
+                request.getDealType()
         );
 
         return findItem.getId();
